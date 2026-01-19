f4ec9bf01efc02237c9f3e5aabb3e6eeb0e1b8c9
yadavdivy007 <yadavdivy007@gmail.com>
2026-01-19 06:21:20 +0000

    Removed unused files and added OOPS pillars example

---
+++ trainingday6_OOPS_pillars.py
@@ -0,0 +1,12 @@
+## OOPS Pillars
+
+#Q1 0 create a ckass student with attirbutes name , roll_no , and marks, write mthod to accept student detaild display student details
+
+
+class Student(self, name, roll_no, marks):
+    def __init__(self):
+        self.name = name
+        self.roll_no = roll_no
+        self.marks = marks
+
+        
